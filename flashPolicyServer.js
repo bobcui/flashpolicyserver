2235011f5ed987fcdec7334f6d72d7e91e7e2b6f
bobcui <bobcui@yeah.net>
2015-01-12 21:09:30 +0800

    add

--- flashPolicyServer.js
+++ flashPolicyServer.js
@@ -1,1 +1,7 @@
-require("./lib/server.js")
+var server = require('policyfile').createServer()
+
+server.listen()
+
+process.on('uncaughtException', function(err){})
+
+console.log("Flash policy server has started.\nPlease see on http://127.0.0.1:843/")
